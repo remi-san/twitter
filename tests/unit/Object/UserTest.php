e99a67f2490643996c1590d69997e659d7e18a6d
Rémi San <remi.san@gmail.com>
2015-04-19 14:38:23 +0200

    finished coverage

--- tests/unit/Object/UserTest.php
+++ tests/unit/Object/UserTest.php
@@ -27,37 +27,6 @@ class UserTest extends \PHPUnit_Framework_TestCase {
         $this->assertEquals($location, $user->getLocation());
         $this->assertEquals($profileHttpUrl, $user->getProfileImageUrl());
         $this->assertEquals($profileHttpsUrl, $user->getProfileImageUrlHttps());
-    }
-
-    /**
-     * @test
-     */
-    public function testSetters()
-    {
-        $id = 42;
-        $name = 'marcel';
-        $screenName = 'Duchamp';
-        $lang = 'fr';
-        $location = 'Paris';
-        $profileHttpUrl = 'http://my.profile.url';
-        $profileHttpsUrl = 'https://my.profile.url';
-
-        $user = new TwitterUser(
-            $id,
-            $screenName,
-            $name,
-            $lang,
-            $location,
-            $profileHttpUrl,
-            $profileHttpsUrl
-        );
-
-        $this->assertEquals($id, $user->getId());
-        $this->assertEquals($name, $user->getName());
-        $this->assertEquals($screenName, $user->getScreenName());
-        $this->assertEquals($lang, $user->getLang());
-        $this->assertEquals($location, $user->getLocation());
-        $this->assertEquals($profileHttpUrl, $user->getProfileImageUrl());
-        $this->assertEquals($profileHttpsUrl, $user->getProfileImageUrlHttps());
+        $this->assertEquals('@'.$screenName, $user->__toString());
     }
 } 
